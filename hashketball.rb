ddd0520854192284a1b048c93904a12b8092f508
James Nicholas Tomashot <ctomashot@gmail.com>
2020-10-18 22:55:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -71,11 +71,11 @@ def game_hash
           number: 4,
           shoe: 18,
           points: 10,
-          rebounds: 12,
-          assists: 12,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 1
+          rebounds: 1,
+          assists: 1,
+          steals: 2,
+          blocks: 7,
+          slam_dunks: 2
         },
                 {
           player_name: "Reggie Evans",
